ccc1273f11783e2bddbba6032cdf4f3df8494dfc
bilolabdunazarov8480gmail.com <bilolabdunazarov8480@gmail.com>
2023-01-20 18:27:56 +0500

    edit name

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo123456',
-      title: 'Flutter Demodsdsdsdsd',
+      title: 'Flutter Bilol',
       theme: ThemeData(
         // This is the theme of your application.
         //
